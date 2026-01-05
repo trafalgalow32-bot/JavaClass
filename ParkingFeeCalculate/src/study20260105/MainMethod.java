e51e86bae0f962c45f052b5f85cbdc6548249297
dw-002 <dw-002@DESKTOP-G635MMJ>
2026-01-05 11:38:19 +0900

    new commit

---
+++ ParkingFeeCalculate/src/study20260105/MainMethod.java
@@ -0,0 +1,44 @@
+package study20260105;
+
+import java.util.Scanner;
+
+public class MainMethod {
+
+	public static void main(String[] args) {
+//		double num = Math.floor(Math.random()*10 + 1);
+//		
+//		System.out.println(num);
+		
+		// 동전의 앞면 뒷면 맞추기
+		
+		Scanner sc = new Scanner(System.in);
+		
+//		int coin = (int)Math.floor(Math.random() * 2) + 1;
+//		// 1 - 앞면, 2 - 뒷면
+//		System.out.println("동전 앞면 뒷면 맞추기");
+//		System.out.print("1-앞면, 2-뒷면 \n 입력 : ");
+//		int user = sc.nextInt();
+//		
+//		if( user == coin ) 
+//			System.out.println("맞추기 성공!!!");
+//		else
+//			System.out.println("맞추기 실패.....");
+	
+		// 주사위
+		
+		int dice = (int)Math.floor(Math.random() * 6) + 1;
+		
+		System.out.println("주사위 맞추기 게임!!!");
+		System.out.print("1~6 중에 하나 입력 : ");
+		int num = sc.nextInt();
+		
+		if (dice == num) 
+			System.out.println("맞추기 성공!!!");
+		else 
+			System.out.println("맞추기 실패");		
+		
+		
+		sc.close();		
+	}
+
+}
