8444b0250df09aa0536f212015de2638f399a0e9
dw-002 <dw-002@DESKTOP-G635MMJ>
2026-01-05 11:52:21 +0900

    modified pw

--- ParkingFeeCalculate/src/study20260105/MainMethod.java
+++ ParkingFeeCalculate/src/study20260105/MainMethod.java
@@ -24,18 +24,35 @@ public class MainMethod {
 //		else
 //			System.out.println("맞추기 실패.....");
 	
-		// 주사위
+		// 주사위 1개
 		
-		int dice = (int)Math.floor(Math.random() * 6) + 1;
+//		int dice = (int)Math.floor(Math.random() * 6) + 1;
+//		
+//		System.out.println("주사위 맞추기 게임!!!");
+//		System.out.print("1~6 중에 하나 입력 : ");
+//		int num = sc.nextInt();
+//		
+//		if (dice == num) 
+//			System.out.println("맞추기 성공!!!");
+//		else 
+//			System.out.println("맞추기 실패");		
 		
-		System.out.println("주사위 맞추기 게임!!!");
-		System.out.print("1~6 중에 하나 입력 : ");
-		int num = sc.nextInt();
+		// 주사위 2개 크기 비교
 		
-		if (dice == num) 
-			System.out.println("맞추기 성공!!!");
-		else 
-			System.out.println("맞추기 실패");		
+		// 컴퓨터 주사위
+		int com = (int)Math.floor(Math.random() * 6 ) + 1;
+		
+		// 나의 주사위
+		int user = (int)Math.floor(Math.random() * 6) + 1;
+		
+		// 내가 컴퓨터보다 큰가? 작은가? 아니면 같은가?
+		if (user > com)
+			System.out.println(" 나의 승!!");
+		else if ( user == com )
+			System.out.println(" 비김....");
+		else
+			System.out.println(" 나의 패배...");
+		
 		
 		
 		sc.close();		
