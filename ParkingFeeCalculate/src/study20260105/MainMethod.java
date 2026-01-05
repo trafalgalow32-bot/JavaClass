1aa3deaf60a410bdc607829b7aae588fc298d3d6
dw-002 <dw-002@DESKTOP-G635MMJ>
2026-01-05 12:26:27 +0900

    Good Afternoon!

--- ParkingFeeCalculate/src/study20260105/MainMethod.java
+++ ParkingFeeCalculate/src/study20260105/MainMethod.java
@@ -8,11 +8,11 @@ public class MainMethod {
 //		double num = Math.floor(Math.random()*10 + 1);
 //		
 //		System.out.println(num);
-		
+
 		// 동전의 앞면 뒷면 맞추기
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 //		int coin = (int)Math.floor(Math.random() * 2) + 1;
 //		// 1 - 앞면, 2 - 뒷면
 //		System.out.println("동전 앞면 뒷면 맞추기");
@@ -23,9 +23,9 @@ public class MainMethod {
 //			System.out.println("맞추기 성공!!!");
 //		else
 //			System.out.println("맞추기 실패.....");
-	
+
 		// 주사위 1개
-		
+
 //		int dice = (int)Math.floor(Math.random() * 6) + 1;
 //		
 //		System.out.println("주사위 맞추기 게임!!!");
@@ -36,26 +36,46 @@ public class MainMethod {
 //			System.out.println("맞추기 성공!!!");
 //		else 
 //			System.out.println("맞추기 실패");		
+
+//		// 주사위 2개 크기 비교
+//
+//		// 컴퓨터 주사위
+//		int com = (int) Math.floor(Math.random() * 6) + 1;
+//
+//		// 나의 주사위
+//		int user = (int) Math.floor(Math.random() * 6) + 1;
+//
+//		// 내가 컴퓨터보다 큰가? 작은가? 아니면 같은가?
+//		if (user > com || (user == 1 && com == 6)) {
+//			if (user == 6 && com == 1)
+//				System.out.println("나의 패배....");
+//			else
+//				System.out.println(" 나의 승!!");
+//		} else if (user == com)
+//			System.out.println(" 비김....");
+//		else
+//			System.out.println(" 나의 패배...");
 		
-		// 주사위 2개 크기 비교
+		// 컴퓨터와 함께 하는 가위바위보 게임!!!!
+		// 1-가위, 2-바위, 3-보
 		
-		// 컴퓨터 주사위
-		int com = (int)Math.floor(Math.random() * 6 ) + 1;
+		// 컴퓨터의 가위바위보는 랜덤으로 
+		// 나의 가위바위보는 키보드로 입력
+		// 내가 이겼는지, 졌는지, 비겼는지 출력하세요.
 		
-		// 나의 주사위
-		int user = (int)Math.floor(Math.random() * 6) + 1;
+		System.out.println("가위바위보!");
+		int user = sc.nextInt();
+//		int user = (int) Math.floor(Math.random() * 3 + 1);
+		int com = (int) Math.floor(Math.random() * 3 + 1);
 		
-		// 내가 컴퓨터보다 큰가? 작은가? 아니면 같은가?
-		if (user > com)
-			System.out.println(" 나의 승!!");
-		else if ( user == com )
-			System.out.println(" 비김....");
+		if ((user == 1 && com == 3) || (user == 2 && com == 1) || (user == 3 && com == 2))
+			System.out.println(" 나의 승!!!");
+		else if (user == com)
+			System.out.println(" 승부는 다음기회로...");
 		else
-			System.out.println(" 나의 패배...");
+			System.out.println(" 내가 졌다...");
 		
-		
-		
-		sc.close();		
+		sc.close();
 	}
 
 }
