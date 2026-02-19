703ab1d8f420a079d178571c3da62437b3e5003c
KHK <trafalgalow32@gmail.com>
2026-02-19 16:03:21 +0900

    freestyle 반영

---
+++ 03.SelfStudy/FreeStyle/FreeStyle.java
@@ -0,0 +1,15 @@
+package FreeStyle;
+
+
+
+public class FreeStyle {
+
+	public static void main(String[] args) {
+		for (int i =2; i <= 9; i++) {
+			System.out.println("구구단 " + i + "단");
+			for (int j = 1; j <= 9; j++) {
+				System.out.println(i + " X " + j + " = " + (i * j));
+			}
+		}
+	}
+}
